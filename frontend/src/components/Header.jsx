150aa452fb756db1f2d97dd822b2dab73713293e
Mike Rushton <mrush94ton@gmail.com>
2024-02-07 19:11:07 +0000

    created header and register ui

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,50 +1,19 @@
 import { Button } from 'flowbite-react'
+import React from 'react'
 import { Link } from 'react-router-dom'
-import Hamburger from 'hamburger-react'
-import { useState } from 'react'
 
 export default function Header() {
-    const [isOpen, setOpen] = useState(false)
   return (
-    <>
-    <navbar className='border-b-2 p-2 flex flex-row justify-around items-center'>
+    <div className='flex flex-row gap-10 justify-center text-white'>
         <Link to='/'>
-            <button className='px-2 py-1 bg-gradient-to-r from-indigo-400 via-purple-300 to-pink-300 rounded-md'>
-                <span className='text-sm sm:text-xl font-semibold text-zinc-700'>User</span>
-                _
-                <span className='text-sm sm:text-xl'>Auth</span>
-            </button>
+            <button className='bg-indigo-500 p-1 rounded-lg'>Home</button>
         </Link>
-        <div className="hidden sm:flex flex-row gap-8">
-            <Link to='/'>
-                <Button className='hover:text-zinc-500'>Home</Button>
-            </Link>
-            <Link to='/signin'>
-                <Button className='hover:text-zinc-500'>Sign-In</Button>
-            </Link>
-            <Link to='/signup'>
-                <Button className='hover:text-zinc-500'>Register</Button>
-            </Link>
-        </div>
-        <div className="flex sm:hidden">
-            <Hamburger toggled={isOpen} toggle={setOpen} />
-        </div>
-    </navbar>
-    {
-        isOpen && (
-            <div className='w-full flex flex-col divide-y-2 justify-center items-center'>
-                <Link to='/'>
-                    <Button className='hover:text-zinc-500'>Home</Button>
-                </Link>
-                <Link to='/signin'>
-                    <Button className='hover:text-zinc-500'>Sign-In</Button>
-                </Link>
-                <Link to='/signup'>
-                    <Button className='hover:text-zinc-500'>Register</Button>
-                </Link>
-            </div>
-        )
-    }
-    </>
+        <Link to='/signin'>
+            <button className='bg-indigo-500 p-1 rounded-lg'>Sign In</button>
+        </Link>
+        <Link to='/signup'>
+            <button className='bg-indigo-500 p-1 rounded-lg'>Sign Up</button>
+        </Link>
+    </div>
   )
 }
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,52 @@
+import { Button, TextInput } from 'flowbite-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Register() {
+
+    const handleSubmit = async () => {
+
+    }
+
   return (
-    <div>Register</div>
+    <div className='min-h-screen flex justify-center items-center'>
+        <form 
+            onSubmit={handleSubmit} 
+            className="w-auto sm:w-96 h-96 p-4 bg-indigo-300 flex flex-col items-center gap-6 rounded-lg"
+        >
+            <h1 className='text-xl'>Register</h1>
+            <div className="flex flex-row items-center gap-2">
+                <label className='text-sm'>Username:</label>
+                <TextInput
+                    type="text" 
+                    placeholder='Enter name'
+                />
+            </div>
+            <div className="flex flex-row items-center gap-2">
+                <label className='text-sm'>Email:</label>
+                <TextInput
+                    type="email" 
+                    placeholder='Enter email'
+                />
+            </div>
+            <div className="flex flex-row items-center gap-2">
+                <label className='text-sm'>Password:</label>
+                <TextInput
+                    type="password" 
+                    placeholder='*******'
+                />
+            </div>
+            <button 
+                className='p-1 rounded-lg bg-indigo-500 px-3 w-20 text-white hover:bg-indigo-700 transition-all duration-100'
+
+            >
+                Register
+            </button>
+            <div className="flex flex-col">
+                <p className='text-sm'>Already have an account?</p>
+                <Link to='/signin' className='text-sm text-blue-500 hover:underline'>Sign In</Link>
+            </div>
+        </form>
+    </div>
   )
 }
