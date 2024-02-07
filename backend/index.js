67279be11ddafdb0f952525de91f0b77fce9ee48
Mike Rushton <mrush94ton@gmail.com>
2024-02-07 16:21:12 +0000

    connected to database and created boilerplate backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,22 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoute from './routes/userRoute.js'
 
 dotenv.config()
 const port = process.env.VITE_PORT
+mongoose.connect(process.env.VITE_DATABASE).then(() => {
+    console.log('Connected to Database')
+}).catch((err) => {
+    console.log(err)
+})
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api/users', userRoute)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
