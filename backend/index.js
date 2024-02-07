b7afc4685d18ec3c521527d98de86354b4e9827c
Mike Rushton <mrush94ton@gmail.com>
2024-02-07 16:27:27 +0000

    installed bcrypt and made custom middleware

--- backend/index.js
+++ backend/index.js
@@ -21,3 +21,14 @@ app.use('/api/users', userRoute)
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
+
+//Layout of error handler
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error'
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
